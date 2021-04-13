4e8f0fd05304afd341fd556b5bd075dfc7bcb08e
christianngouw <christiangouw168@gmail.com>
2021-04-14 00:10:54 +0700

    add new route for get feed from post

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -25,6 +27,8 @@ Route::get("/user_information/{id}", [UserController::class, 'userInformation'])
 Route::post("/user", [UserController::class, 'store']);
 Route::put('/user/{id}', [UserController::class, 'update']);
 Route::delete('/user/{id]', [UserController::class, 'destroy']);
+
+Route::get('/post', [PostController::class, 'index']);
 
 
 Route::post("/login", [UserController::class, 'login']);
