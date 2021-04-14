9b538a200427a771fc63255fac6a050d4a359fc7
christianngouw <christiangouw168@gmail.com>
2021-04-14 23:49:56 +0700

    configure Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,6 +9,8 @@ class Post extends Model
 {
     use HasFactory;
 
+    protected $hidden = ['created_at', 'updated_at'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
