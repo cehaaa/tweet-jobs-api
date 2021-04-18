1abbf603cc7aa24dd87207650a6cf78c5bb58769
christianngouw <christiangouw168@gmail.com>
2021-04-18 12:10:00 +0700

    code refactoring

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,7 +9,9 @@ class Post extends Model
 {
     use HasFactory;
 
-    protected $hidden = ['created_at', 'updated_at'];
+
+
+    // protected $hidden = ['created_at', 'updated_at'];
 
     public function user()
     {
